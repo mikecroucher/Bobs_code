7a4dbff01c96ec19bf0cc76cb5a737228dda94a4
Michael Croucher <michael.p.croucher@googlemail.com>
2019-11-03 23:41:26 +0000

    Bob pointed out that we are multiplying matrices element wise!

--- simulation.m
+++ simulation.m
@@ -2,8 +2,10 @@ disp('This is the first version of the code')
 % Let's multiply two matrices together
 a = rand(10);
 b = rand(10);
-c = a.*b;
+c = a*b;
 
 % Fixed the broken thing.  MATLAB is case sensitive and I had a large C
 % instead of a small c
-d = c + 1
+d = c + 1;
+
+
